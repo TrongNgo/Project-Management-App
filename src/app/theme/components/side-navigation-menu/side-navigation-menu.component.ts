6cd9cb02c46b8e89da4007f483c1dcc5af528a06
Trong Ngo <trong.ngo@enlabsoftware.com>
2020-02-12 19:34:22 +0700

    implement header

--- src/app/theme/components/side-navigation-menu/side-navigation-menu.component.ts
+++ src/app/theme/components/side-navigation-menu/side-navigation-menu.component.ts
@@ -11,51 +11,46 @@ import * as events from 'devextreme/events';
   templateUrl: './side-navigation-menu.component.html',
   styleUrls: ['./side-navigation-menu.component.scss']
 })
-export class SideNavigationMenuComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild(DxTreeViewComponent, {static: true}) menu: DxTreeViewComponent;
+export class SideNavigationMenuComponent implements OnInit {
+  // @ViewChild(DxTreeViewComponent, {static: true}) menu: DxTreeViewComponent;
+  //
+  // @Output()
+  // selectedItemChanged = new EventEmitter<string>();
+  //
+  // @Output()
+  // openMenu = new EventEmitter<any>();
+  //
+  // @Input()
+  // set selectedItem(value: string) {
+  //   if (this.menu.instance) {
+  //     this.menu.instance.selectItem(value);
+  //   }
+  // }
 
-  @Output()
-  selectedItemChanged = new EventEmitter<string>();
-
-  @Output()
-  openMenu = new EventEmitter<any>();
-
-  @Input()
-  set selectedItem(value: string) {
-    if (this.menu.instance) {
-      this.menu.instance.selectItem(value);
-    }
-  }
-
-  private _compactMode = false;
-  @Input()
-  get compactMode() {
-    return this._compactMode;
-  }
-
-  set compactMode(val) {
-    this._compactMode = val;
-    if (val && this.menu.instance) {
-      this.menu.instance.collapseAll();
-    }
-  }
+  // private _compactMode = false;
+  // @Input()
+  // get compactMode() {
+  //   return this._compactMode;
+  // }
+  //
+  // set compactMode(val) {
+  //   this._compactMode = val;
+  //   if (val && this.menu.instance) {
+  //     this.menu.instance.collapseAll();
+  //   }
+  // }
 
   // TODO: Edit the menu here
-  items: any[] = [
+  pages = [
     {
-      text: 'My Details',
-      path: '/my-details',
-      icon: 'home'
+      routerLink: '/',
+      text: 'Projects',
+      icon: '<i class="fas fa-store"></i>'
     },
     {
-      text: 'Examples',
-      icon: 'folder',
-      items: [
-        {
-          text: 'Profile',
-          path: '/profile'
-        }
-      ]
+      routerLink: 'customer',
+      text: 'Customer',
+      icon: '<i class="fas fa-list-alt"></i>'
     }
   ];
 
@@ -64,43 +59,43 @@ export class SideNavigationMenuComponent implements OnInit, AfterViewInit, OnDes
 
   ngOnInit() {
   }
-
-  updateSelection(event) {
-    const nodeClass = 'dx-treeview-node';
-    const selectedClass = 'dx-state-selected';
-    const leafNodeClass = 'dx-treeview-node-is-leaf';
-    const element: HTMLElement = event.element;
-
-    const rootNodes = element.querySelectorAll(`.${nodeClass}:not(.${leafNodeClass})`);
-    Array.from(rootNodes).forEach(node => {
-      node.classList.remove(selectedClass);
-    });
-
-    let selectedNode = element.querySelector(`.${nodeClass}.${selectedClass}`);
-    while (selectedNode && selectedNode.parentElement) {
-      if (selectedNode.classList.contains(nodeClass)) {
-        selectedNode.classList.add(selectedClass);
-      }
-
-      selectedNode = selectedNode.parentElement;
-    }
-  }
-
-  onItemClick(event) {
-    this.selectedItemChanged.emit(event);
-  }
-
-  onMenuInitialized(event) {
-    event.component.option('deferRendering', false);
-  }
-
-  ngAfterViewInit() {
-    events.on(this.elementRef.nativeElement, 'dxclick', (e) => {
-      this.openMenu.next(e);
-    });
-  }
-
-  ngOnDestroy() {
-    events.off(this.elementRef.nativeElement, 'dxclick');
-  }
+  //
+  // updateSelection(event) {
+  //   const nodeClass = 'dx-treeview-node';
+  //   const selectedClass = 'dx-state-selected';
+  //   const leafNodeClass = 'dx-treeview-node-is-leaf';
+  //   const element: HTMLElement = event.element;
+  //
+  //   const rootNodes = element.querySelectorAll(`.${nodeClass}:not(.${leafNodeClass})`);
+  //   Array.from(rootNodes).forEach(node => {
+  //     node.classList.remove(selectedClass);
+  //   });
+  //
+  //   let selectedNode = element.querySelector(`.${nodeClass}.${selectedClass}`);
+  //   while (selectedNode && selectedNode.parentElement) {
+  //     if (selectedNode.classList.contains(nodeClass)) {
+  //       selectedNode.classList.add(selectedClass);
+  //     }
+  //
+  //     selectedNode = selectedNode.parentElement;
+  //   }
+  // }
+  //
+  // onItemClick(event) {
+  //   this.selectedItemChanged.emit(event);
+  // }
+  //
+  // onMenuInitialized(event) {
+  //   event.component.option('deferRendering', false);
+  // }
+  //
+  // ngAfterViewInit() {
+  //   events.on(this.elementRef.nativeElement, 'dxclick', (e) => {
+  //     this.openMenu.next(e);
+  //   });
+  // }
+  //
+  // ngOnDestroy() {
+  //   events.off(this.elementRef.nativeElement, 'dxclick');
+  // }
 }
