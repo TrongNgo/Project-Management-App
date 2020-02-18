a55357d4ca866ead44889a4b2205cdaff7457128
trong.ngotrung@gmail.com <trong.ngotrung@gmail.com>
2020-02-18 23:03:58 +0700

    Fix Custom Pagination UI

--- src/app/modules/project/project-list/project-list.component.ts
+++ src/app/modules/project/project-list/project-list.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 
-import {DxListComponent} from 'devextreme-angular';
+import {DxButtonComponent, DxListComponent} from 'devextreme-angular';
 import DataSource from 'devextreme/data/data_source';
 
 @Component({
@@ -11,107 +11,66 @@ import DataSource from 'devextreme/data/data_source';
 
 export class ProjectListComponent implements OnInit {
     @ViewChild('list', {static: false}) list: DxListComponent;
+    @ViewChild('prevBtn', { static: false }) prevBtn: DxButtonComponent;
+    @ViewChild('nextBtn', { static: false }) nextBtn: DxButtonComponent;
 
-    listData: any;
+    dataSource: DataSource;
+    // listData: any;
     items: any[];
+    pageSize: number = 2;
+    totalProjectCount: number;
+    maxPage: number;
 
     constructor() {
-        this.listData = new DataSource({
-            store: [
-              'Prepare 2016 Financial',
-              'Prepare 2016 Marketing Plan',
-              'Update Personnel Files',
-              'Review Health Insurance Options Under the Affordable Care Act',
-              'New Brochures',
-              '2016 Brochure Designs',
-              'Brochure Design Review',
-              'Website Re-Design Plan'
-            ],
-            sort: 'ProductID',
-            paginate: true,
-            pageSize: 2
-        });
-
-        this.listData.load().done(r => {
-        this.items = r;
-        });
     }
 
     ngOnInit() {
-    }
+        this.dataSource = new DataSource({
+            store: [
+                'Prepare 2016 Financial',
+                'Prepare 2016 Marketing Plan',
+                'Update Personnel Files',
+                'Review Health Insurance Options Under the Affordable Care Act',
+                'New Brochures',
+                '2016 Brochure Designs',
+                'Brochure Design Review',
+                'Website Re-Design Plan'
+            ],
+            sort: 'ProductID',
+            paginate: true,
+            pageSize: this.pageSize,
+            requireTotalCount: true
+        });
 
-    nextClick(e) {
-        const currentIndex = this.listData.pageIndex();
-
-        this.listData.pageIndex(currentIndex + 1);
-        this.listData.load().done(r => {
-          this.items = r;
+        this.dataSource.load().then((items, extra) => {
+            this.totalProjectCount = extra.totalCount;
+            this.maxPage = Math.ceil(this.totalProjectCount / this.pageSize);
         });
     }
 
-    prevClick(e) {
-        const currentIndex = this.listData.pageIndex();
-        if (currentIndex > 0) {
-            this.listData.pageIndex(currentIndex - 1);
-            this.listData.load().done(r => {
-              this.items = r;
-            });
+    nextClick(e) {
+        const newIndex = this.dataSource.pageIndex() + 1;
+        if (newIndex === (this.maxPage - 1)) {
+            this.nextBtn.instance.option('disabled', true);
         }
+        this.prevBtn.instance.option('disabled', false);
+        // go to next page
+        this.dataSource.pageIndex(newIndex);
+        this.changePage();
+    }
+
+    prevClick(e) {
+        const prevIndex = this.dataSource.pageIndex() - 1;
+        if (prevIndex === 0) {
+            this.prevBtn.instance.option('disabled', true);
+        }
+        this.nextBtn.instance.option('disabled', false);
+
+        this.dataSource.pageIndex(prevIndex);
+        this.changePage();
+    }
+
+    changePage() {
+        this.dataSource.load();
     }
 }
-
-/*
-*   @ViewChild("prevBtn", { static: false }) prevBtn: DxButtonComponent;
-  @ViewChild("nextBtn", { static: false }) nextBtn: DxButtonComponent;
-  @ViewChild("list", { static: false }) dxlist: DxListComponent;
-
-  dataSource: DataSource;
-
-  selectedItems: any[] = [];
-  allowDeleting: boolean = false;
-  deleteType: string = "toggle";
-  tasks: string[];
-
-  constructor(service: Service) {
-    this.tasks = service.getTasks();
-  }
-
-  ngOnInit() {
-    console.log("this", this.tasks);
-    const a = this.tasks;
-    console.log("list", this.dxlist.instance);
-    this.dataSource = new DataSource({
-      store: a,
-      paginate: true,
-      pageSize: 2
-    });
-    this.changePage();
-  }
-
-  onPrevButton(e) {
-    var newIndex = this.dataSource.pageIndex() - 1;
-    if (newIndex === 0) {
-      e.component.option("disabled", true);
-    }
-    this.nextBtn.instance.option("disabled", false);
-    this.dataSource.pageIndex(newIndex);
-    this.changePage();
-  }
-
-  onNextButton(e) {
-    var newIndex = this.dataSource.pageIndex() + 1;
-    this.prevBtn.instance.option("disabled", false);
-    this.dataSource.pageIndex(newIndex);
-    this.changePage();
-  }
-
-  changePage() {
-    this.dataSource.load().done(function(items) {
-      console.log("list", this.list);
-      this.list.instance.option("dataSource", items);
-      if (items.length < 2) {
-        // this.nextBtn.instance.option("disabled", true);
-      }
-    });
-  }
-* */
