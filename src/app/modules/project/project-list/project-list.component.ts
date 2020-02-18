e64cb822f0a2cd401f71d0ce246c6b11681702fc
trong.ngotrung@gmail.com <trong.ngotrung@gmail.com>
2020-02-18 07:10:18 +0700

    Edit ProjectList UI

--- src/app/modules/project/project-list/project-list.component.ts
+++ src/app/modules/project/project-list/project-list.component.ts
@@ -59,3 +59,59 @@ export class ProjectListComponent implements OnInit {
         }
     }
 }
+
+/*
+*   @ViewChild("prevBtn", { static: false }) prevBtn: DxButtonComponent;
+  @ViewChild("nextBtn", { static: false }) nextBtn: DxButtonComponent;
+  @ViewChild("list", { static: false }) dxlist: DxListComponent;
+
+  dataSource: DataSource;
+
+  selectedItems: any[] = [];
+  allowDeleting: boolean = false;
+  deleteType: string = "toggle";
+  tasks: string[];
+
+  constructor(service: Service) {
+    this.tasks = service.getTasks();
+  }
+
+  ngOnInit() {
+    console.log("this", this.tasks);
+    const a = this.tasks;
+    console.log("list", this.dxlist.instance);
+    this.dataSource = new DataSource({
+      store: a,
+      paginate: true,
+      pageSize: 2
+    });
+    this.changePage();
+  }
+
+  onPrevButton(e) {
+    var newIndex = this.dataSource.pageIndex() - 1;
+    if (newIndex === 0) {
+      e.component.option("disabled", true);
+    }
+    this.nextBtn.instance.option("disabled", false);
+    this.dataSource.pageIndex(newIndex);
+    this.changePage();
+  }
+
+  onNextButton(e) {
+    var newIndex = this.dataSource.pageIndex() + 1;
+    this.prevBtn.instance.option("disabled", false);
+    this.dataSource.pageIndex(newIndex);
+    this.changePage();
+  }
+
+  changePage() {
+    this.dataSource.load().done(function(items) {
+      console.log("list", this.list);
+      this.list.instance.option("dataSource", items);
+      if (items.length < 2) {
+        // this.nextBtn.instance.option("disabled", true);
+      }
+    });
+  }
+* */
